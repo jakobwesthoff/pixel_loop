f11019439c7be638a9d63cb04a5232b5ba585fcc
Jakob Westhoff <jakob@westhoffswelt.de>
2024-08-27 20:18:17 +0200

    Implement very basic fps render benchmarking

--- src/trivial_cli_demo/main.rs
+++ src/trivial_cli_demo/main.rs
@@ -1,3 +1,5 @@
+use std::time::SystemTime;
+
 use anyhow::Result;
 use crossterm::terminal;
 use pixel_loop::crossterm_canvas::CrosstermCanvas;
@@ -7,6 +9,8 @@ struct State {
     box_position: (i64, i64),
     box_direction: (i64, i64),
     box_size: (u32, u32),
+    frame_count: usize,
+    start_frame_time: SystemTime,
 }
 
 impl State {
@@ -15,6 +19,8 @@ impl State {
             box_position: Default::default(),
             box_direction: (1, 1),
             box_size: (20, 10),
+            frame_count: 0,
+            start_frame_time: SystemTime::now(),
         }
     }
 }
@@ -59,6 +65,10 @@ fn main() -> Result<()> {
             Ok(())
         },
         |e, s, canvas, dt| {
+            if cfg!(feature = "benchmark_fps") {
+                s.frame_count += 1;
+            }
+
             let yellow = Color::from_rgb(255, 255, 128);
             let dark_yellow = Color::from_rgb(128, 128, 64);
 
@@ -82,6 +92,23 @@ fn main() -> Result<()> {
             // RENDER END
 
             canvas.render()?;
+
+            if cfg!(feature = "benchmark_fps") {
+                let duration = s.start_frame_time.elapsed().unwrap();
+                let secs = duration.as_secs();
+
+                if secs >= 5 {
+                    let nanos = duration.as_nanos();
+                    let fps = 1_000_000_000f64 / (nanos / s.frame_count as u128) as f64;
+                    eprintln!(
+                        "Rendered {frame_count} frames in {secs_fraction}s resulting in {fps} fps.",
+                        frame_count = s.frame_count,
+                        secs_fraction =
+                            secs as f64 + duration.subsec_micros() as f64 / 1_000_000f64
+                    );
+                    std::process::exit(0);
+                }
+            }
             Ok(())
         },
     )?;
